86600995d665daae352241389f21b7a2988b971c
Niall Cunningham <N.CUNNINGHAM12@nuigalway.ie>
2018-10-22 23:39:52 +0100

    News source wide analysis
    The script now outputs a graph of an entire news source over all it's
    articles

--- HTML_Metadata.py
+++ HTML_Metadata.py
@@ -6,26 +6,47 @@ import json
 from os import listdir
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
+from enum import Enum
 
 stopwords = set(stopwords.words('english'))
-source = "The New York Times"
-date = "2017-04-19"
-month = "April"
-files = listdir("C:/Users/Niall/Desktop/FYP/JSON Data/" + month + "/" + date + "/" + source)
+source = "FrontPage Magazine"
+#date = "2017-04-19"
+#start_month = "April"
+
+months = listdir("C:/Users/Niall/Desktop/FYP/JSON Data/")
+
+class Article:
+    def __init__(self, month, dates):
+        self.month = month
+        self.dates = dates
+
+articles = []
+
+for i in months:
+    days = listdir("C:/Users/Niall/Desktop/FYP/JSON Data/" + i)
+    a1 = Article(i, days)
+    articles.append(a1)
 
 NYT_APR_19 = []
 data = []
 i = 0
 #print(files)
-for files[i] in files:
+for articles[i] in articles:
+    for day in articles[i].dates:
+        try:
+            article_headline = listdir("C:/Users/Niall/Desktop/FYP/JSON Data/" + articles[i].month + "/" + day + "/" + source)
+        except FileNotFoundError:
+            print("No artilces published by "+ source + " on " + day)
+            break
+        for headline in article_headline:
+            with open ("C:/Users/Niall/Desktop/FYP/JSON Data/" + articles[i].month + "/" + day + "/" + source + "/" + headline, 'rb') as f:
+                NYT =json.load(f)
+                data.append(NYT['html'])
     i += 1
-    with open ("C:/Users/Niall/Desktop/FYP/JSON Data/" + month + "/" + date + "/" + source + "/" + files[0], 'rb') as f:
-        NYT =json.load(f)
-        data.append(NYT['html'])
 
-all_NYT_data = ' '.join(data)
+all_news_source_data = ' '.join(data)
 #print(all_NYT_data)
-word_tokens = word_tokenize(all_NYT_data)
+word_tokens = word_tokenize(all_news_source_data)
 
 #print(word_tokens[52][1])
 
@@ -37,8 +58,12 @@ for w in word_tokens:
 
 #print (tags)
 freq_dist = FreqDist(tags)
-print(freq_dist.most_common(25))
+
+print(freq_dist.most_common(20))
+
 print(freq_dist)
+
+freq_dist.plot(20, title = source + " Distribution of top 20 HTML tags across all data")
 
 #filtered_sentence = []
 
