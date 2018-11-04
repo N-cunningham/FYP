53faf93668a86342ea14429a53f4946b3ee1187c
Niall Cunningham <N.CUNNINGHAM12@nuigalway.ie>
2018-11-04 23:35:18 +0000

    started utilities & added source-wide Aaalysis

--- Tilte_Word_Freq.py
+++ Tilte_Word_Freq.py
@@ -5,9 +5,13 @@ from os import listdir
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 import xlsxwriter
+import Utilities
+from Utilities import get_sources
+from Utilities import get_nouns
 
 stopwords = set(stopwords.words('english'))
 source = "RT"
+sources = get_sources()
 months = listdir("C:/Users/Niall/Desktop/FYP/JSON Data/")
 
 class Article:
@@ -25,32 +29,36 @@ for i in months:
 data = []
 i = 0
 #print(files)
-for articles[i] in articles:
-    for day in articles[i].dates:
-        try:
-            article_headline = listdir("C:/Users/Niall/Desktop/FYP/JSON Data/" + articles[i].month + "/" + day + "/" + source)
-        except FileNotFoundError:
-            print("No artilces published by "+ source + " on " + day)
-            break
-        for headline in article_headline:
-            with open ("C:/Users/Niall/Desktop/FYP/JSON Data/" + articles[i].month + "/" + day + "/" + source + "/" + headline, 'rb') as f:
-                NYT =json.load(f)
-                data.append(NYT['title'])
-    i += 1
+for s in sources:
+    i = 0
+    for a in articles:
+        for day in articles[i].dates:
+            try:
+                article_headline = listdir("C:/Users/Niall/Desktop/FYP/JSON Data/" + a.month + "/" + day + "/" + s)
+            except FileNotFoundError:
+                print("No artilces published by "+ s + " on " + day)
+                break
+            for headline in article_headline:
+                with open ("C:/Users/Niall/Desktop/FYP/JSON Data/" + a.month + "/" + day + "/" + s + "/" + headline, 'rb') as f:
+                    NYT =json.load(f)
+                    data.append(NYT['title'])
+        i += 1
 
 all_news_source_data = ' '.join(data)
 word_tokens = word_tokenize(all_news_source_data)
 
 filtered_titles = []
-additional_stopords = [":", "'", "'s", "The", "-", "?", ",", '"', '”', '“', "'", "'", "'", "'", "$", ]# TODO Come back to investiagte use of punctuation marks
+additional_stopords = [":", "'", "'s", "The", "-", "?", ",", '"', '”', '“', "'", "'", "'", "'", "$", ")", "(", "_", "&", '...', '.', '�', ';', '!']# TODO Come back to investiagte use of punctuation marks
 
-for w in word_tokens:
+nouns = get_nouns(word_tokens)
+
+for w in nouns:
     if w not in stopwords and w not in additional_stopords:
        filtered_titles.append(w)
 
 freq_dist = FreqDist(filtered_titles)
-freq_dist_top_40 = freq_dist.most_common(40)
-print(freq_dist_top_40)
+freq_dist_top_100= freq_dist.most_common(100)
+print(freq_dist_top_100)
 print(freq_dist)
 
 freq_dist.plot(20, title = source + " Distribution of top 20 title words across all data")
---
+++ Utilities.py
@@ -0,0 +1,50 @@
+import nltk
+import json
+from os import listdir
+from nltk.tokenize import word_tokenize
+
+months = listdir("C:/Users/Niall/Desktop/FYP/JSON Data/")
+
+class Article:
+    def __init__(self, month, dates):
+        self.month = month
+        self.dates = dates
+
+days_with_articles = []
+
+for m in months:
+    days = listdir("C:/Users/Niall/Desktop/FYP/JSON Data/" + m)
+    a1 = Article(m, days)
+    days_with_articles.append(a1)
+
+
+def get_nouns(words):
+
+    nouns = []
+
+    for w in words:
+        tagged_word = nltk.pos_tag(w)
+        if (tagged_word[0][1] == 'NN' or tagged_word[0][1] == 'NNP' or tagged_word[0][1] == 'NNS' or tagged_word[0][1] == 'NNPS'):
+                 nouns.append(w)
+    return nouns
+
+
+
+def get_sources():
+    data = []
+    article_sources_all = []
+    i = 0
+    #print(files)
+    for days_with_articles[i] in days_with_articles:
+        for day in days_with_articles[i].dates:
+            try:
+                article_sources = listdir("C:/Users/Niall/Desktop/FYP/JSON Data/" + days_with_articles[i].month + "/" + day + "/")
+                for a in article_sources:
+                    if a not in article_sources_all:
+                        article_sources_all.append(a)
+            except FileNotFoundError:
+                print("No artilces published by "+ source + " on " + day)
+                break
+        i += 1
+
+    return article_sources_all
