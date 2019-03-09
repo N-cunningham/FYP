62b82643296c445b2c7d535fdfcd7aa6ff334360
Niall Cunningham <N.CUNNINGHAM12@nuigalway.ie>
2019-03-09 21:42:57 +0000

    Serialization integreated in testing

--- HTML_Metadata.py
+++ HTML_Metadata.py
@@ -11,7 +11,8 @@ from Utilities import get_sources
 from Utilities import save_as_JSON
 import Utilities
 from sklearn.metrics.pairwise import cosine_similarity
-
+import pickle
+import os
 
 stopwords = set(stopwords.words('english'))
 additional_stopords = [":", "'", "'s", "The", "-", "?", ",", '"', '”', '“', "'", "'", "'", "'", "$", ")", "(", "_", "&", '...', '.', '�', ';', '!', "''", "``", "%", "@", "--", ".", "[", "]", "[]", "[ ]", "’", "|", "‘", "'", ".", " ", "e", "i", "a", "r", "."]# TODO Come back to investiagte use of punctuation marks
@@ -23,21 +24,22 @@ sourcesAlreadyDone = ["CNBC", "BBC"]
 #sources = get_sources()
 
 
-reliableSources = ["AP", "The Daily Beast", "Media Matters for America", "The Fiscal Times", "CNBC", "Talking Points Memo"]
+reliableSources = ["BBC", "CBS News", "CNBC", "NPR", "PBS"]#, "Slate"]#, "The Atlantic"]#, "The Daily Beast", "The Hill", "Salon", "The Huffington Post", "The New York Times", "USA Today", "Vox", "Washington Examiner", "Media Matters for America", "The Fiscal Times", "AP", "Talking Points Memo"]
 for rs in reliableSources:
     sources.append(rs)
 reliableTags = []
 
-quesionableSources = ["Alternative Media Syndicate", "Breitbart", "Freedom Daily", "Liberty Writers", "Infowars", "Intellihub", "FrontPage Magazine"]
+quesionableSources = ["Alternative Media Syndicate", "Bipartisan Report", "Breitbart", "CNS News", "Conservative Tribune", "Freedom Daily"]#, "Liberty Writers"]#, "Hang The Bankers", "Infowars", "Intellihub", "FrontPage Magazine"]#, "Occupy Democrats", "Politicus USA", "Prntly","RedState", "The Duran", "TruthFeed", "USA Politics Now", "End the Fed", "NODISINFO", "Freedom Outpost", "Waking Times", "NewsBusters", "Activist Post", "The D.C. Clothesline","World News Politics", "Daily Buzz Live", "DC Gazette"]
 for qs in quesionableSources:
     sources.append(qs)
 quesionableTags = []
 
-satireSources = ["The Beaverton" , "The Chaser", "The Shovel", "The Spoof" , "Glossy News", "Newslo", "Humor Times", "The Burrard Street Journal", "The Borowitz Report", "The Huffington Post Political Satire", "National Report"]
+satireSources = satireSources = ["The Beaverton" , "The Chaser", "The Shovel", "The Spoof" , "Glossy News", "Newslo", "Humor Times", "The Burrard Street Journal", "The Borowitz Report", "The Huffington Post Political Satire", "National Report"]
 for ss in satireSources:
     sources.append(ss)
 satireTags = []
 
+sourcesTags = []
 
 #sources.append(source)
 #date = "2017-04-19"
@@ -58,117 +60,192 @@ for i in months:
 
 data = []
 for s in sources:
-    del data[:]
-    warning = ""
-    tags = []
-    words = " "
-    article_data = " "
-    time1 = time.localtime()
-    print(str(time1.tm_hour) + ":" + str(time1.tm_min) + ":" + str(time1.tm_sec) + ": Processing articles in " + s + "...")
-    aCounter = 0
-    for a in articles:
+    filename = "C:/Users/Niall/Desktop/FYP/Output JSON Data/HTML/Pickle/"+ s + ".pickle"
+    if os.path.isfile(filename) == False:
+        del data[:]
+        warning = ""
+        tags = []
+        words = " "
+        article_data = " "
         time1 = time.localtime()
-        if aCounter == 1:
-            print(str(time1.tm_hour) + ":" + str(time1.tm_min) + ":" + str(time1.tm_sec) + ": 14% done")
+        print(str(time1.tm_hour) + ":" + str(time1.tm_min) + ":" + str(time1.tm_sec) + ": Processing articles in " + s + "...")
+        aCounter = 0
+        for a in articles:
+            time1 = time.localtime()
+            if aCounter == 1:
+                print(str(time1.tm_hour) + ":" + str(time1.tm_min) + ":" + str(time1.tm_sec) + ": 14% done")
 
-        if aCounter == 2:
-            print(str(time1.tm_hour) + ":" + str(time1.tm_min) + ":" + str(time1.tm_sec) + ": 29% done")
+            if aCounter == 2:
+                print(str(time1.tm_hour) + ":" + str(time1.tm_min) + ":" + str(time1.tm_sec) + ": 29% done")
 
-        if aCounter == 4:
-            print(str(time1.tm_hour) + ":" + str(time1.tm_min) + ":" + str(time1.tm_sec) + ": 57% done")
+            if aCounter == 4:
+                print(str(time1.tm_hour) + ":" + str(time1.tm_min) + ":" + str(time1.tm_sec) + ": 57% done")
 
-        if aCounter == 5:
-            print(str(time1.tm_hour) + ":" + str(time1.tm_min) + ":" + str(time1.tm_sec) + ": 71% done")
+            if aCounter == 5:
+                print(str(time1.tm_hour) + ":" + str(time1.tm_min) + ":" + str(time1.tm_sec) + ": 71% done")
 
-        if aCounter == 6:
-            print(str(time1.tm_hour) + ":" + str(time1.tm_min) + ":" + str(time1.tm_sec) + ": 86% done")
+            if aCounter == 6:
+                print(str(time1.tm_hour) + ":" + str(time1.tm_min) + ":" + str(time1.tm_sec) + ": 86% done")
 
-        aCounter = aCounter + 1
+            aCounter = aCounter + 1
 
-        for day in a.dates:
+            for day in a.dates:
 
-            file_exists = "true"
+                file_exists = "true"
+                try:
+                    article_headline = listdir("C:/Users/Niall/Desktop/FYP/JSON Data/" + a.month + "/" + day + "/" + s)
+                except FileNotFoundError:
+                    #print("No artilces published by "+ s + " on " + day)
+                    file_exists = "false"
+
+                if file_exists != "false":
+
+                    for headline in article_headline:
+                        article_data = []
+                        with open ("C:/Users/Niall/Desktop/FYP/JSON Data/" + a.month + "/" + day + "/" + s + "/" + headline, 'rb') as f:
+                            try:
+                                del article_data
+                                article_data = json.load(f)
+                                del words
+                                words = word_tokenize(article_data['html'])
+                                last = " "
+                                for w in words:
+                                    if len(w) > 1 and w[0] == "/" and w[1] != "/" and last == "<":#THis line detects HTML tags
+                                        data.append(w)
+                                    last = w
+                            except MemoryError:
+                                print(' \n ***Too many aticles from ' + s + ' to batch process*** \n')
+                                warning = "INCOMPLETE"
+                                break
+                            except ValueError:
+                                print(' \n ***Decoding failed for '+ a.month + "/" + day + "/" + s + "/" + headline  + ' *** \n')
+
+
+        all_news_source_data = []
+        del all_news_source_data[:]
+        word_tokens = []
+        del word_tokens[:]
+
+        if warning != "INCOMPLETE":
+            time2 = time.localtime()
+            print(str(time2.tm_hour) + ":" + str(time2.tm_min) + ":" + str(time2.tm_sec) + ": Tokenizing article HTML tags in " + s + "...")
             try:
-                article_headline = listdir("C:/Users/Niall/Desktop/FYP/JSON Data/" + a.month + "/" + day + "/" + s)
-            except FileNotFoundError:
-                #print("No artilces published by "+ s + " on " + day)
-                file_exists = "false"
+                all_news_source_data = ' '.join(map(str, data))
+                word_tokens = word_tokenize(all_news_source_data)
 
-            if file_exists != "false":
+            except MemoryError:
+                print(' \n ***Too many aticles from ' + s + ' to batch process*** \n')
+                warning = "INCOMPLETE"
+                save_as_JSON( [], 'HTML/'+ 'INCOMPLETE_' + s + '_top_100_.json')
+                save_as_JSON( [], 'HTML/'+ 'INCOMPLETE_' + s + '_Frequency_Distribution.json')
 
-                for headline in article_headline:
-                    article_data = []
-                    with open ("C:/Users/Niall/Desktop/FYP/JSON Data/" + a.month + "/" + day + "/" + s + "/" + headline, 'rb') as f:
-                        try:
-                            del article_data
-                            article_data = json.load(f)
-                            del words
-                            words = word_tokenize(article_data['html'])
-                            last = " "
-                            for w in words:
-                                if len(w) > 1 and w[0] == "/" and w[1] != "/" and last == "<":#THis line detects HTML tags
-                                    data.append(w)
-                                last = w
-                        except MemoryError:
-                            print(' \n ***Too many aticles from ' + s + ' to batch process*** \n')
-                            warning = "INCOMPLETE"
-                            break
-                        except ValueError:
-                            print(' \n ***Decoding failed for '+ a.month + "/" + day + "/" + s + "/" + headline  + ' *** \n')
+    #print(all_NYT_data)
+        if warning != "INCOMPLETE":
+            #print (tags)
+            freq_dist = FreqDist(word_tokens)
+            freq_dist_top_100 = freq_dist.most_common(100)
 
+            print(s)
+            freqDistsNames.append(s)
+            print(freq_dist_top_100)
 
-    all_news_source_data = []
-    del all_news_source_data[:]
-    word_tokens = []
-    del word_tokens[:]
+            pickle_out = open("C:/Users/Niall/Desktop/FYP/Output JSON Data/HTML/Pickle/"+ s + ".pickle","wb")
+            pickle.dump(word_tokens, pickle_out)
+            del pickle_out
+            pickle_out.close()
 
-    if warning != "INCOMPLETE":
-        time2 = time.localtime()
-        print(str(time2.tm_hour) + ":" + str(time2.tm_min) + ":" + str(time2.tm_sec) + ": Tokenizing article HTML tags in " + s + "...")
-        try:
-            all_news_source_data = ' '.join(map(str, data))
-            word_tokens = word_tokenize(all_news_source_data)
+            filename = "C:/Users/Niall/Desktop/FYP/Output JSON Data/HTML/Pickle/"+ s + ".pickle"
+            pickle_in = open(filename,"rb")
+            test_deSer = pickle.load(pickle_in)
 
-        except MemoryError:
-            print(' \n ***Too many aticles from ' + s + ' to batch process*** \n')
-            warning = "INCOMPLETE"
-            save_as_JSON( [], 'HTML/'+ 'INCOMPLETE_' + s + '_top_100_.json')
-            save_as_JSON( [], 'HTML/'+ 'INCOMPLETE_' + s + '_Frequency_Distribution.json')
+            sourcesTags.append(word_tokens)
 
-#print(all_NYT_data)
-    if warning != "INCOMPLETE":
-        #print (tags)
-        freq_dist = FreqDist(word_tokens)
+            print("\n")
+            save_as_JSON(freq_dist_top_100, 'HTML/' + s +'_top_100.json')
+            save_as_JSON(freq_dist,  'HTML/' + s + '_Frequency_Distribution.json')
+            del pickle_out
+            del test_deSer
+    else:
+        pickle_in = open(filename,"rb")
+        word_tokens_pickle = pickle.load(pickle_in)
+
+        freq_dist = FreqDist(word_tokens_pickle)
         freq_dist_top_100 = freq_dist.most_common(100)
 
-        print(s)
+        print(s + " Already processed")
         freqDistsNames.append(s)
         print(freq_dist_top_100)
-
-        if s in reliableSources:
-            reliableTags.append(word_tokens)
-        elif s in quesionableSources:
-            quesionableTags.append(word_tokens)
-        elif s in satireSources:
-            satireTags.append(word_tokens)
-
+        sourcesTags.append(word_tokens_pickle)
 
         print("\n")
         save_as_JSON(freq_dist_top_100, 'HTML/' + s +'_top_100.json')
         save_as_JSON(freq_dist,  'HTML/' + s + '_Frequency_Distribution.json')
+        del pickle_in
+        del word_tokens_pickle
+
+SatireCorrect = 0
+QuesionableCorrect = 0
+ReliableCorrect = 0
+index = 0
+for st in sources:
+
+    reliableTags = []
+    quesionableTags = []
+    satireTags = []
+
+    index2 = 0
+    for sss in sources:
+        if sss in reliableSources and st != sss:
+            reliableTags.append(sourcesTags[index2])
+        elif sss in quesionableSources and st != sss:
+            quesionableTags.append(sourcesTags[index2])
+        elif sss in satireSources and st != sss:
+            satireTags.append(sourcesTags[index2])
+        index2 = index2 + 1
+
+    reliableTags = ' '.join(map(str, reliableTags))
+    quesionableTags = ' '.join(map(str, quesionableTags))
+    satireTags = ' '.join(map(str, satireTags))
+
+    print("\n" + sources[index])
+
+    #st = ' '.join(map(str, st))
+
+    thisSource = ' '.join(map(str, sourcesTags[index]))
+    ReliableScore = Utilities.get_cosine_sim(reliableTags, thisSource)[1][0]
+    QuesionableScore = Utilities.get_cosine_sim(quesionableTags, thisSource)[1][0]
+    SatireScore = Utilities.get_cosine_sim(satireTags, thisSource)[1][0]
+
+    print("Reliable:\t" + str(ReliableScore))
+
+    print("Quesionable:\t"+ str(QuesionableScore))
+
+    print("Satire:\t\t"+ str(SatireScore))
+
+    if st in reliableSources and ReliableScore > QuesionableScore and ReliableScore > SatireScore:
+        ReliableCorrect = ReliableCorrect + 1
+
+    if st in quesionableSources and QuesionableScore > ReliableScore and QuesionableScore > SatireScore:
+        QuesionableCorrect = QuesionableCorrect + 1
+
+    if st in satireSources and SatireScore > QuesionableScore and SatireScore > ReliableScore:
+        SatireCorrect = SatireCorrect + 1
+
+    index = index + 1
+
+    del thisSource
+    del reliableTags
+    del quesionableTags
+    del satireTags
 
 
-reliableTagsAll = ' '.join(map(str, reliableTags))
-quesionableTagsAll = ' '.join(map(str, quesionableTags))
-satireTagsAll = ' '.join(map(str, satireTags))
+print("\nReliable score: " + str((ReliableCorrect / len(reliableSources))   *   100) + "%")
+print("Quesionable score: " + str((QuesionableCorrect / len(quesionableSources))     *   100) + "%")
+print("Satire score: " + str((SatireCorrect / len(satireSources))   *   100) + "%")
+print("\nOverall score: " + str(((ReliableCorrect + QuesionableCorrect + SatireCorrect) / (len(sources)))   *   100) + "%")
 
-print("Reliable")
-print(Utilities.get_cosine_sim(reliableTagsAll, satireTagsAll))
 
-print("\nQuesionable")
-print(Utilities.get_cosine_sim(quesionableTagsAll, satireTagsAll))
-
-print("Finito")
+print("\nFinito")
 #filtered_sentence = []
 
 #for w in word_tokens:
