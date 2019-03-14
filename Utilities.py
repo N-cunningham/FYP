f471644ac1f8c493ebcc686b3345e2d1dcf0851e
Niall Cunningham <nccunningham@hotmail.com>
2019-03-14 18:20:52 +0000

    Update Utilities.py

--- Utilities.py
+++ Utilities.py
@@ -1,96 +1,42 @@
-import nltk
-import json
-from os import listdir
-from nltk.tokenize import word_tokenize
-from nltk.stem.porter import *
-from collections import Counter
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
+Bootstrap: docker
+From: tensorflow/tensorflow:1.9.0-gpu-py3
 
-months = listdir("C:/Users/Niall/Desktop/FYP/JSON Data/")
+%environment
+  # use bash as default shell
+  SHELL=/bin/bash
+  export SHELL
 
-class Article:
-    def __init__(self, month, dates):
-        self.month = month
-        self.dates = dates
+%setup
+  # runs on host - the path to the image is $SINGULARITY_ROOTFS
 
-days_with_articles = []
+%post
+  # post-setup script
 
-for m in months:
-    days = listdir("C:/Users/Niall/Desktop/FYP/JSON Data/" + m)
-    a1 = Article(m, days)
-    days_with_articles.append(a1)
+  # load environment variables
+  . /environment
 
-def get_cosine_sim(*strs):
-    vectors = [t for t in get_vector(*strs)]
-    return cosine_similarity(vectors)
+  # use bash as default shell
+  echo 'SHELL=/bin/bash' >> /environment
 
-def get_all_vectors(*strs):
-    vectors = [t for t in get_vector(*strs)]
-    return vectors
+  # make environment file executable
+  chmod +x /environment
 
-def get_vector(*strs):
-    text = [t for t in strs]
-    vectorizer = CountVectorizer(text)
-    vectorizer.fit(text)
-    ret = vectorizer.transform(text).toarray()
-    del text
-    del vectorizer
-    return ret
+  # default mount paths
+  mkdir /scratch /data
 
-def get_verbs(words):
-
-    verbs = []
-
-    for w in words:
-        tagged_word = nltk.pos_tag(w)
-        if (tagged_word[0][1] == 'RB' or tagged_word[0][1] == 'RBR' or tagged_word[0][1] == 'RBS' ):
-                 verbs.append(w)
-    for a in verbs:
-        print("# " + a)
-    return verbs
+  # additional packages
+  apt-get update
+  apt-get install -y python-tk
+  apt-get install -y python3-numpy python3-scipy
+  apt-get install -y python3-pip python3-dev build-essential
 
 
-def get_nouns(words):
+  pip install scikit-learn
+  pip install nltk
 
-    nouns = []
+%runscript
+  # executes with the singularity run command
+  # delete this section to use existing docker ENTRYPOINT command
 
-    for w in words:
-        tagged_word = nltk.pos_tag(w)
-        if (tagged_word[0][1] == 'NN' or tagged_word[0][1] == 'NNP' or tagged_word[0][1] == 'NNS' or tagged_word[0][1] == 'NNPS'):
-                 nouns.append(w)
-    return nouns
-
-
-def save_as_JSON(list, file_end):
-
-    with open('C:/Users/Niall/Desktop/FYP/Output JSON Data/' + file_end, 'w') as outfile:
-        json.dump(list, outfile)
-
-
-def get_sources():
-    data = []
-    article_sources_all = []
-    i = 0
-
-    for days_with_articles[i] in days_with_articles:
-        for day in days_with_articles[i].dates:
-            try:
-                article_sources = listdir("C:/Users/Niall/Desktop/FYP/JSON Data/" + days_with_articles[i].month + "/" + day + "/")
-                for a in article_sources:
-                    if a not in article_sources_all:
-                        article_sources_all.append(a)
-            except FileNotFoundError:
-                print("No artilces published by "+ source + " on " + day)
-                break
-        i += 1
-
-    return article_sources_all
-
-
-def stem_tokens(tokens):
-    stemmer = PorterStemmer()
-    stemmed = []
-    for item in tokens:
-        stemmed.append(stemmer.stem(item))
-    return stemmed
+%test
+  # test that script is a success
